127682ddf0bf48b8b6605757ad2c91496f0095c8
ROllerozxa <rollerozxa@voxelmanip.se>
2024-11-24 00:11:52 +0100

    Helper functions for getting mouse state

---
+++ src/mouse.c
@@ -0,0 +1,17 @@
+#include "mouse.h"
+
+int mouse_get_state_scaled(SDL_Renderer *renderer, float *x, float *y) {
+	SDL_FPoint mouse;
+	int clicked = SDL_GetMouseState(&mouse.x, &mouse.y);
+	SDL_RenderCoordinatesFromWindow(renderer, mouse.x, mouse.y, &mouse.x, &mouse.y);
+	*x = mouse.x;
+	*y = mouse.y;
+
+	return clicked;
+}
+
+SDL_FPoint mouse_get_point(SDL_Renderer *renderer) {
+	SDL_FPoint mouse;
+	SDL_GetMouseState(&mouse.x, &mouse.y);
+	return mouse;
+}
---
+++ src/mouse.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+int mouse_get_state_scaled(SDL_Renderer *renderer, float *x, float *y);
+SDL_FPoint mouse_get_point(SDL_Renderer *renderer);
