1f3f83838b55f7c329379b2426a12848f86379f4
ROllerozxa <rollerozxa@voxelmanip.se>
2024-12-17 23:36:45 +0100

    Minor code cleanups of the font renderer

--- src/font.c
+++ src/font.c
@@ -1,6 +1,5 @@
 #include "font.h"
 #include "font_bitmap.h"
-#include "consts.h"
 
 SDL_Texture* font_tex;
 
@@ -25,9 +24,6 @@ SDL_Texture* load_font(SDL_Renderer *renderer) {
 	return texture;
 }
 
-#define FGRID_WIDTH (FONT_WIDTH/GLYPH_WIDTH)
-#define FGRID_HEIGHT (FONT_HEIGHT/GLYPH_HEIGHT)
-
 static SDL_Color font_color;
 
 SDL_Color get_font_color(void) {
@@ -40,18 +36,18 @@ void set_font_color(SDL_Color clr) {
 }
 
 void draw_char(SDL_Renderer *renderer, unsigned char character, int cx, int cy, int scale) {
-	int cell_x = (character-32) % FGRID_WIDTH;
-	int cell_y = (character-32) / FGRID_WIDTH;
+	SDL_Point cell = {
+		(character-32) % (FONT_WIDTH/GLYPH_WIDTH),
+		(character-32) / (FONT_WIDTH/GLYPH_WIDTH)
+	};
 
-	if (font_tex == NULL) {
+	if (font_tex == NULL)
 		font_tex = load_font(renderer);
-	}
 
-	SDL_RenderTexture(
-		renderer, font_tex,
-		RECT(cell_x*GLYPH_WIDTH, cell_y*GLYPH_HEIGHT, GLYPH_WIDTH, GLYPH_HEIGHT),
-		RECT(cx, cy, GLYPH_WIDTH*scale, GLYPH_HEIGHT*scale)
-	);
+	SDL_FRect srcrect = {cell.x * GLYPH_WIDTH, cell.y * GLYPH_HEIGHT, GLYPH_WIDTH, GLYPH_HEIGHT};
+	SDL_FRect dstrect = {cx, cy, GLYPH_WIDTH * scale, GLYPH_HEIGHT * scale};
+
+	SDL_RenderTexture(renderer, font_tex, &srcrect, &dstrect);
 }
 
 void draw_char_shadow(SDL_Renderer *renderer, unsigned char character, int cx, int cy, int scale) {
@@ -65,7 +61,7 @@ void draw_char_shadow(SDL_Renderer *renderer, unsigned char character, int cx, i
 
 void draw_text(SDL_Renderer *renderer, const char* text, int x, int y, int scale) {
 	for (size_t i = 0; text[i] != '\0'; i++) {
-		draw_char(renderer, text[i], x+i*GLYPH_WIDTH*scale, y, scale);
+		draw_char(renderer, text[i], x + i * GLYPH_WIDTH * scale, y, scale);
 	}
 }
 
