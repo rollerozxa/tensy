039af723c6745f6d44ba770889a65818dfef72a3
ROllerozxa <rollerozxa@voxelmanip.se>
2025-02-05 22:03:07 +0100

    Initial sound system, add proper button click sound

---
+++ src/sound.c
@@ -0,0 +1,24 @@
+#include "sound.h"
+
+#include <data/click.h>
+
+#include <SDL3_mixer/SDL_mixer.h>
+
+static Mix_Chunk* sound_bank[100];
+static bool sound_loaded = false;
+
+#define LOAD_SOUND(id, var) \
+	sound_bank[id] = Mix_LoadWAV_IO(SDL_IOFromMem(var, var##_len), true)
+
+void sound_init() {
+	LOAD_SOUND(SND_CLICK, click_ogg);
+
+	sound_loaded = true;
+}
+
+void sound_play(int sound_id) {
+	if (!sound_loaded)
+		sound_init();
+
+	Mix_PlayChannel(-1, sound_bank[sound_id], 0);
+}
---
+++ src/sound.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+enum {
+	SND_CLICK
+};
+
+void sound_play(int sound_id);
