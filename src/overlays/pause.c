88097d8a8c9c25a1229d3918feeef7e369fb03b7
ROllerozxa <rollerozxa@voxelmanip.se>
2025-05-26 00:20:20 +0200

    Add save button to pause menu

--- src/overlays/pause.c
+++ src/overlays/pause.c
@@ -4,18 +4,24 @@
 #include "gui/button.h"
 #include "overlay.h"
 #include "render.h"
+#include "savestate.h"
 #include "scene.h"
 
-static Button resume_button, exit_button;
+static Button resume_button, save_button, exit_button;
 
 void pause_init(void) {
 	BUTTON(resume_button, RECT(0, 0, 150, 40), "Resume");
+	BUTTON(save_button, RECT(0, 0, 150, 40), "Save");
 	BUTTON(exit_button, RECT(0, 0, 150, 40), "Exit");
 }
 
 void pause_event(const SDL_Event *ev) {
 	if (button_event(ev, &resume_button))
 		hide_overlay();
+
+	if (button_event(ev, &save_button)) {
+		savestate_save();
+	}
 
 	if (button_event(ev, &exit_button)) {
 		hide_overlay();
@@ -31,12 +37,19 @@ void pause_draw(SDL_Renderer *renderer) {
 	SDL_FRect text_rect = {pausebg_rect.x, pausebg_rect.y, pausebg_rect.w, 20*2};
 	draw_text_shadow_centered(renderer, "Game paused", &text_rect, 2);
 
-	resume_button.rect.x = pausebg_rect.x + CENTER(pausebg_rect.w, resume_button.rect.w);
-	resume_button.rect.y = pausebg_rect.y + 20*4;
+	float btn_x = pausebg_rect.x + CENTER(pausebg_rect.w, resume_button.rect.w);
+
+	resume_button.rect.x = btn_x;
+	resume_button.rect.y = pausebg_rect.y + 20*3;
 
 	button(renderer, &resume_button);
 
-	exit_button.rect.x = pausebg_rect.x + CENTER(pausebg_rect.w, exit_button.rect.w);
+	save_button.rect.x = btn_x;
+	save_button.rect.y = pausebg_rect.y + 20*6;
+
+	button(renderer, &save_button);
+
+	exit_button.rect.x = btn_x;
 	exit_button.rect.y = pausebg_rect.y + 20*9;
 
 	button(renderer, &exit_button);
