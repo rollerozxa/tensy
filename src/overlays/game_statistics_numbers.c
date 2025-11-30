8af634b8f7a8f248bbe31a53f2ecb1277b776916
ROllerozxa <rollerozxa@voxelmanip.se>
2025-11-30 20:03:38 +0100

    Add title to in-game statistics dialog

--- src/overlays/game_statistics_numbers.c
+++ src/overlays/game_statistics_numbers.c
@@ -3,6 +3,7 @@
 #include "gamestate.h"
 #include "graph.h"
 #include "overlay.h"
+#include "text.h"
 
 static Button back_button;
 
@@ -17,6 +18,17 @@ void game_statistics_numbers_event(const SDL_Event *ev) {
 
 void game_statistics_numbers_draw(void) {
 	draw_translucent_overlay();
+
+	SDL_FRect rect = {
+		(float)SCREEN_W / 2 - (SCREEN_W * 0.8f) / 2,
+		0,
+		SCREEN_W * 0.8f,
+		45
+	};
+	draw_set_color(0x071a45);
+	draw_fill_rect(&rect);
+
+	text_draw_shadow("Per-number game statistics", 80, 20, 2);
 
 	uint32_t *settings_numbers = game.number_stats;
 	graph_draw(settings_numbers);
