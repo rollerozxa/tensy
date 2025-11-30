ac34e58b679c7e6bd71cf1a235ad66abe6a225e0
ROllerozxa <rollerozxa@voxelmanip.se>
2025-11-30 20:10:57 +0100

    Tensy 1.0.1

--- src/version_info.h
+++ src/version_info.h
@@ -2,7 +2,7 @@
 
 #define VER_MAJOR 1
 #define VER_MINOR 0
-#define VER_PATCH 0
-#define VER_DEV true
+#define VER_PATCH 1
+#define VER_DEV false
 
-#define VER_STRING "1.0"
+#define VER_STRING "1.0.1"
