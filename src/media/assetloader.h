eaf3aff1c4498ca4e26d7be290a0a5b28f369a99
ROllerozxa <rollerozxa@voxelmanip.se>
2026-01-15 00:05:24 +0100

    Fix compile error for embedded data builds

--- src/media/assetloader.h
+++ src/media/assetloader.h
@@ -16,7 +16,7 @@
 	#define ASSETLOADER_MUSIC(data, path) MIX_LoadAudio_IO(mixer, SDL_IOFromMem(data, data##_len), false, false)
 	#define ASSETLOADER_SOUND(data, path) MIX_LoadAudio_IO(mixer, SDL_IOFromMem(data, data##_len), true, false)
 	#define ASSETLOADER_QOI(data, path) SDL_LoadQOI_IO(SDL_IOFromMem(data, data##_len))
-	#define ASSETLOADER_PNG(data, path) SDL_LoadPNG_IO(SDL_IOFromMem(data, data##_len))
+	#define ASSETLOADER_PNG(data, path) SDL_LoadPNG_IO(SDL_IOFromMem(data, data##_len), false)
 
 	#define EMBEDDED_DATA true
 #endif
